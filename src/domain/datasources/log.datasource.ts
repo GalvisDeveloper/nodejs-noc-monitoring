8b98303d9e3ffc0352cedfab1defb1df8e27de58
GalvisDev <galvisdeveloper@gmail.com>
2024-06-12 20:09:56 -0500

    Datasources y repository

---
+++ src/domain/datasources/log.datasource.ts
@@ -0,0 +1,9 @@
+import { LogEntity, LogSeverityLevel } from "../entities/log.entity";
+
+
+export abstract class LogDataSource {
+
+    abstract saveLogs(log: LogEntity): Promise<void>;
+    abstract getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]>;
+
+}
