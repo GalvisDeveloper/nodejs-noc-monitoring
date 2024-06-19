e5d66bd8072ae32e41656552eb18dd7b79964dbc
GalvisDev <galvisdeveloper@gmail.com>
2024-06-18 22:21:52 -0500

    Conexion a mongo con mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
-import { Server } from "./presentation/server"
+import { envs } from "./config/plugins/envs.plugin";
+import { MongoDataSource } from "./data/mongo";
 
-const main = () => {
-    Server.start();
+const main = async () => {
+    await MongoDataSource.connect({
+        mongoUrl: envs.MONGO_URL,
+        dbName: envs.MONGO_DB_NAME
+    })
+    // Server.start();
 }
 
 (async () => {
---
+++ src/data/mongo/init.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+
+interface ConnectionsOptions {
+    mongoUrl: string;
+    dbName: string;
+}
+
+
+export class MongoDataSource {
+
+    static async connect(options: ConnectionsOptions) {
+        const { mongoUrl, dbName } = options;
+        try {
+            await mongoose.connect(mongoUrl, {
+                dbName,
+            })
+
+            console.log('Mongo connected')
+
+        } catch (error) {
+            console.log('Mongo connection error')
+            throw error;
+        }
+    }
+
+}
